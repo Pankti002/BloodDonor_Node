8e84d34fca6ee87a1beb69b892a7f299d7b034d6
Pankti002 <panktishah8902@gmail.com>
2023-03-24 14:31:28 +0530

    CRUD

---
+++ index.js
@@ -0,0 +1,36 @@
+const express=require("express")
+const mongoose= require("mongoose")
+const donorController=require("./Controller/donorcontroller")
+
+const app=express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//donor Api
+app.get("/donor",donorController.getAllDonors)
+app.post("/donor",donorController.addDonor)
+app.put("/donor",donorController.updateDonor)
+app.delete("/donor",donorController.deletedonor)
+
+mongoose.connect("mongodb://localhost/blooddonor2023",function(err){
+    if(err)
+    {
+        console.log(err)
+        console.log("Something Went Wrong....")
+    }
+    else{
+        console.log("db Connected!!")
+    }
+ })
+
+app.listen(9001,function(err){
+    if(err)
+    {
+        console.log(err)
+        console.log("Something Went Wrong....")
+    }
+    else{
+        console.log("Server Connected at port number 9001")
+    }
+})
